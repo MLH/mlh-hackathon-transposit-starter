2892725c386d4b7c20b7e835a58bf6b3b0f7f4d9
Eddie Salce <eddie@majorleaguehacking.com>
2019-04-10 13:48:44 +0000

    Initial service boilerplate

---
+++ src/hello_world.js
@@ -0,0 +1,17 @@
+(params) => {
+  return [
+    {
+      language: "english",
+      message: "Hello, world"
+    },
+    {
+      language: "spanish",
+      message: "Hola, mundo"
+    }
+  ];
+}
+
+/*
+ * For sample code and reference material, visit
+ * https://docs.transposit.com/references/js-operations
+ */
